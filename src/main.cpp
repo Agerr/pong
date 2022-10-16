3807129e2681da38e9a90ade22dd0643c598ee78
Agerr <agerrrr@proton.me>
2022-10-16 13:22:39 +0300

    Improved walls

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,19 @@ int main()
     window.setVerticalSyncEnabled(true);
 
     // Walls
-    sf::RectangleShape wallTop { sf::Vector2f(1600, 10) };
-    sf::RectangleShape wallBot { sf::Vector2f(1600, 10) };
-    sf::RectangleShape wallLeft { sf::Vector2f(10, 1000) };
-    sf::RectangleShape wallRight { sf::Vector2f(10, 1000) };
+    sf::RectangleShape wallTop { sf::Vector2f(1600, 15) };
+    sf::RectangleShape wallBot { sf::Vector2f(1600, 15) };
+    sf::RectangleShape wallLeft { sf::Vector2f(15, 1000) };
+    sf::RectangleShape wallRight { sf::Vector2f(15, 1000) };
+    sf::RectangleShape wallMid { sf::Vector2f(10, 30) };
 
     wallTop.setPosition(0, 0);
-    wallBot.setPosition(0, 990);
+    wallBot.setPosition(0, 985);
     wallLeft.setPosition(0, 0);
-    wallRight.setPosition(1590, 0);
+    wallRight.setPosition(1585, 0);
+    wallMid.setPosition(795, 0);
 
+    float midWallCount = 970 / 30.f / 2.f;
 
     // Font
     sf::Font font;
@@ -34,7 +37,7 @@ int main()
            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
         }
 
-        // Render
+        // Rendering
         window.clear();
 
         // Render walls
@@ -42,7 +45,15 @@ int main()
         window.draw(wallBot);
         window.draw(wallLeft);
         window.draw(wallRight);
+        
+        // Render middle wall
+        for (int i = 0; i < midWallCount; i++)
+        {
+            wallMid.setPosition(795, 60 * i);
+            window.draw(wallMid);
+        }
 
+        // Display everything
         window.display();
     }
 
