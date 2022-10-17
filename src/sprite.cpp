94d3b09a2b9c5fc424f93fec05460ed4cec1aba9
Agerr <agerrrr@proton.me>
2022-10-17 16:13:36 +0300

    Removed comment lol

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -12,7 +12,7 @@ void Sprite::move(Player player1, Player player2)
     if (pos.y < 15 || pos.y > 935) vel_y *= -1;
 
     // Player 2 collision
-    bool inX2 { ((pos.x + 50) >= player2.pos.x) && ((pos.x + 50) < (player2.pos.x + vel_x)) };                                    // This line is fucked up somehow
+    bool inX2 { ((pos.x + 50) >= player2.pos.x) && ((pos.x + 50) < (player2.pos.x + vel_x)) };
     bool inY2 { ((pos.y + 50) > player2.pos.y) && (pos.y < player2.pos.y + 100) };
     if (inX2 && inY2) vel_x *= -1;
 
