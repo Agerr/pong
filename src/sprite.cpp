16f5e7405d5edea5758f3317c7202d631bcc82a4
Agerr <agerrrr@proton.me>
2022-10-17 18:15:53 +0300

    Added speed mechanic

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -12,14 +12,16 @@ void Sprite::move(Player &player1, Player &player2)
     {
         player1.score++;
         pos = sf::Vector2f(775, 475);
-        vel_x = -vel_x;
+        vel_x = -3;
+        vel_y = 3;
     }
 
     if (pos.x < 15)
     {
         player2.score++;
         pos = sf::Vector2f(775, 475);
-        vel_x = -vel_x;
+        vel_x = 3;
+        vel_y = -3;
     }
 
     // Top and Bottom
@@ -28,13 +30,20 @@ void Sprite::move(Player &player1, Player &player2)
     // Player 1 collision
     bool inX1 { (pos.x <= (player1.pos.x + 15)) && (pos.x > (player1.pos.x + 15 + vel_x)) };
     bool inY1 { ((pos.y + 50) > player1.pos.y) && (pos.y < (player1.pos.y + 100)) };
-    if (inX1 && inY1) vel_x *= -1;
-
+    if (inX1 && inY1) 
+    {
+        vel_x *= -1.1;
+        vel_y *= 1.1;
+    }
 
     // Player 2 collision
     bool inX2 { ((pos.x + 50) >= player2.pos.x) && ((pos.x + 50) < (player2.pos.x + vel_x)) };
     bool inY2 { ((pos.y + 50) > player2.pos.y) && (pos.y < player2.pos.y + 100) };
-    if (inX2 && inY2) vel_x *= -1;
+    if (inX2 && inY2)
+    {
+        vel_x *= -1.1;
+        vel_y *= 1.1;
+    }
 
     pos.x += vel_x;
     pos.y += vel_y;
